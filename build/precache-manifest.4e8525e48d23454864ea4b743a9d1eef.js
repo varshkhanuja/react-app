90f0b6d3467e1eb1bc8572f4304ab07c08584f65
Varsha Khanuja <varsha.khanuja@capitalnumbers.com>
2020-04-14 02:20:42 +0530

    adding project

---
+++ build/precache-manifest.4e8525e48d23454864ea4b743a9d1eef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e9fcce3430080e3b9941e99f005e70d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bafad2175103aea7ce69",
+    "url": "/static/css/main.6b542ac4.chunk.css"
+  },
+  {
+    "revision": "e154f4cf7e613bd909ea",
+    "url": "/static/js/2.231fd2f3.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.231fd2f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bafad2175103aea7ce69",
+    "url": "/static/js/main.28f83baa.chunk.js"
+  },
+  {
+    "revision": "4b40cfa1f2cd2ffb143f",
+    "url": "/static/js/runtime-main.aab238ca.js"
+  }
+]);
